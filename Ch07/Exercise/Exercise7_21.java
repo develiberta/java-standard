0ea23ee626d70076ad35532cd1c0a58bb9250402
develiberta <develiberta@gmail.com>
2020-05-01 18:52:17 +0900

    에러 수정

--- Ch07/Exercise/Exercise7_21.java
+++ Ch07/Exercise/Exercise7_21.java
@@ -1,5 +1,11 @@
 /* Exercise 7-21. 다음과 같이 attack메서드가 정의되어 있을 때,
  * 이 메서드의 매개변수로 가능한 것 두 가지를 적으시오.
+ * interface Movable {
+ *	void movd(int x, int y);
+ * } 
+ * void attack(Movable f) {
+ *	// 내용 생략
+ * }
  *
  * [답]
  * 1. Movable인터페이스를 구현한 클래스
@@ -8,12 +14,4 @@
 class Exercise7_21 {
 	public static void main(String args[]) {
 	}
-}
-
-interface Movable {
-	void move(int x, int y);
-}
-
-void attack(Movable f) {
-	/* 내용 생략 */
 }
