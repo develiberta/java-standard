4b01acbe2f127521b8b3d84f8948320afd63dcfc
develiberta <develiberta@gmail.com>
2020-05-23 21:36:32 +0900

    Ch09 java.lang패키지와 유용한클래스 기본문제(31~32)

---
+++ Ch09/Example/RegularEx2.java
@@ -0,0 +1,20 @@
+import java.util.regex.*;
+
+class RegularEx2 {
+	public static void main(String[] args) {
+		String[] data = {"bat", "baby", "bonus", "c", "cA", "ca", "co", "c.", "c0", "c#", "car", "combat", "count", "date", "disc"};
+		
+		String[] pattern = {".*", "c[a-z]*", "c[a-z]", "c[a-zA-Z]", "c[a-zA-Z0-9]", "c.", "c.*", "c\\.", "c\\w", "c\\d", "c.*t", "[b|c].*", ".*a.*", ".*a.+", "[b|c].{2}"};
+		
+		for(int x=0; x<pattern.length; x++) {
+			Pattern p = Pattern.compile(pattern[x]);
+			System.out.print("Pattern : " + pattern[x] + " 결과 : ");
+			for(int i=0; i<data.length; i++) {
+				Matcher m = p.matcher(data[i]);
+				if(m.matches())
+					System.out.print(data[i]+",");
+			}
+			System.out.println();
+		}
+	}	// public static void main(String[] args)
+}
