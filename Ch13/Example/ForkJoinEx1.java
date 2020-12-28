c320293d8c87c39c9a513ef1f6dec1080a28ad99
seungha.lee <seungha.lee@uclick.co.kr>
2020-12-28 23:15:41 +0900

    Ch13 쓰레드 thread : 기본문제(27) -- 완

---
+++ Ch13/Example/ForkJoinEx1.java
@@ -0,0 +1,68 @@
+import java.util.concurrent.*;
+
+class ForkJoinEx1 {
+	static final ForkJoinPool pool = new ForkJoinPool();	// 스레드 풀을 생성
+	
+	
+	public static void main(String[] args) {
+		long from = 1L, to = 100_000_000L;
+		
+		SumTask task = new SumTask(from, to);
+		
+		long start = System.currentTimeMillis();	// 시작시간 초기화
+		Long result = pool.invoke(task);
+		
+		System.out.println("Elapsed time(4 Core) : "
+							+ (System.currentTimeMillis() - start));				
+		System.out.printf("sum of %d ~ %d = %d%n", from, to, result);
+		System.out.println();
+		
+		result = 0L;
+		start = System.currentTimeMillis();	// 시작시간 초기화
+		
+		for (long i=from; i<=to; i++) {
+			result += i;
+		}
+		
+		System.out.println("Elapsed time(1 Core) : "
+							+ (System.currentTimeMillis() - start));
+		System.out.printf("sum of %d ~ %d = %d%n", from, to, result);
+	}
+}
+
+class SumTask extends RecursiveTask<Long> {
+	long from, to;
+	
+	SumTask(long from, long to) {
+		this.from	= from;
+		this.to		= to;
+	}
+	
+	public Long compute() {
+		long size = to - from + 1;	// from <= i <= to
+		
+		if (size <= 5) {	// 더할 숫자가 5개 이하이면
+			return sum();	// 숫자의 합을 반환
+		}
+		
+		long half = (from + to) / 2;
+		
+		// 범위를 반으로 나눠서 두 개의 작업을 생성
+		SumTask leftSum = new SumTask(from, half);
+		SumTask rightSum = new SumTask(half + 1, to);
+		
+		leftSum.fork();
+		
+		return rightSum.compute() + leftSum.join();
+	}
+	
+	long sum() {	// from ~ to 의 모든 숫자를 더한 결과를 반환
+		long tmp = 0L;
+		
+		for (long i=from; i<=to; i++) {
+			tmp += i;
+		}
+		
+		return tmp;
+	}
+}
