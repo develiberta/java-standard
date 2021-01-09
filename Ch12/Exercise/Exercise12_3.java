c8d71a3b7c43572bfe76526cd7c69a8a3820e185
develiberta <develiberta@gmail.com>
2021-01-09 23:12:43 +0900

    Ch12 지네릭스, 열거형, 애너테이션 연습문제 -- 채점(1~7) -- 완료

--- Ch12/Exercise/Exercise12_3.java
+++ Ch12/Exercise/Exercise12_3.java
@@ -9,7 +9,14 @@
  * f. Box<? extends Fruit> b = new Box<Apple>();
  * g. Box<? extends Object> b = new Box<? extends Fruit>();
  *
- * [정답] a, b, d, e, g
+ * [정답] a, b,    d, e, g(틀림)
+ * [정답]       c, d,    g
+ *
+ * [해설]
+ * a. (바람직하지는 않음) Box<?> b = new Box<>();
+ * b. (바람직)
+ * d. (바람직하지는 않음) Box<?> b = new Box<Fruit>();
+ * f. (바람직)
  * 
  */
 class Box<T extends Fruit> {	// 지네릭 타입 T를 선언
